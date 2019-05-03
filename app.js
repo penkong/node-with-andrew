f1c5ad0b218a9f2505a1f9be341a446306ee3d22
mazdak <nazemi.works@gmail.com>
2019-05-03 10:38:33 -0700

    addnode to json-db ,yargs,chalk,argv,try,catch

--- app.js
+++ app.js
@@ -5,12 +5,75 @@
 // const validator = require('validator');
 // console.log(validator.isURL('https://www.sdf.com')); is email also
 
-const getNotes = require('./notes');
-const msg = getNotes();
-console.log(msg);
-// -----------
-const chalk = require('chalk');
-console.log(chalk.rgb(155, 200, 204).inverse('Hello!'));
-console.log(chalk.red('Hello!'));
+// const msg = getNotes();
+// console.log(msg);
+// // -----------
+// console.log(chalk.rgb(155, 200, 204).inverse('Hello!'));
+// console.log(chalk.red('Hello!'));
 // ----------
 // nodemon is automate node refresher package help not invoke repeatedly
+// -------------------------
+// props of command line are in process
+// console.log(process.argv[2]);
+// --------------
+// const command = process.argv[2];
+// console.log(process.argv);
+// if (command === 'add') {
+//   console.log('adding note');
+// } else if (command === 'remove') {
+//   console.log('removing node');
+// }
+// console.log(process.argv);
+
+// customize  yargs version
+const chalk = require('chalk');
+const yargs = require('yargs');
+const notes = require('./notes');
+
+yargs.version('1.1.0');
+// create add command
+yargs.command({
+  command: 'add',
+  describe: 'add a new node',
+  builder: {
+    title: {
+      describe: 'Node title',
+      demandOption: true, //make it require to receive title
+      type: 'string' //always need string to receive
+    },
+    body: {
+      describe: 'body of node',
+      demandOption: true,
+      type: 'string'
+    }
+  },
+  handler: function (argv) {
+    notes.addNote(argv.title, argv.body);
+  }
+})
+// create remove command
+yargs.command({
+  command: 'remove',
+  describe: 'remove new note',
+  handler: function () {
+    console.log('removed new node');
+  }
+})
+// read
+yargs.command({
+  command: 'read',
+  describe: 'read new note',
+  handler: function () {
+    console.log('read all nodes');
+  }
+})
+// list
+yargs.command({
+  command: 'list',
+  describe: 'list new note',
+  handler: function () {
+    console.log('list all nodes');
+  }
+})
+
+yargs.parse(); // equal console.log(yargs.argv);
--- notes.js
+++ notes.js
@@ -1,5 +1,42 @@
+const fs = require('fs');
+
 const getNotes = function () {
   return 'tour notes ....';
 }
+// ----------------------------------------------------
+const loadNotes = () => {
+  try {
+    const dataBuffer = fs.readFileSync('notes.json');
+    const dataJson = dataBuffer.toString();
+    return JSON.parse(dataJson);
+  } catch (e) {
+    return [];
+  }
+}
 
-module.exports = getNotes;
+const saveNotes = notes => {
+  const dataJSON = JSON.stringify(notes);
+  fs.writeFileSync('notes.json', dataJSON);
+}
+
+const addNote = (title, body) => {
+  const notes = loadNotes();
+  const duplicateNotes = notes.filter(note =>
+    note.title === title
+  )
+
+  if (!duplicateNotes.length) {
+    notes.push({
+      title: title,
+      body: body
+    })
+    saveNotes(notes);
+  } else {
+    console.log('title taken');
+  }
+}
+
+module.exports = {
+  getNotes,
+  addNote
+};
